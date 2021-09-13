1f733c03a22e007dcc125417769f9a586dd3bbe0
nezaki <r.nezaki@gmail.com>
2021-09-13 23:27:21 +0900

    no message

---
+++ alembic/dev/versions/a55349f71b7b_initialize.py
@@ -0,0 +1,29 @@
+"""initialize
+
+Revision ID: a55349f71b7b
+Revises: 
+Create Date: 2021-09-13 22:31:23.552901
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a55349f71b7b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'project',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(50), nullable=False),
+        sa.Column('description', sa.Unicode(200)),
+    )
+
+
+def downgrade():
+    op.drop_table('project')
