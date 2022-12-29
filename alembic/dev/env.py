0de343460cc02c49999c826064d75ee0ecce3f25
Ryosuke Nezaki <r.nezaki@gmail.com>
2022-12-29 15:25:26 +0900

    no message

--- alembic/dev/env.py
+++ alembic/dev/env.py
@@ -1,91 +1,57 @@
 import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
-from sqlalchemy.engine import Connection
+from sqlalchemy import engine_from_config, pool, text
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = None
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+schema = config.get_main_option("schema")
+url = config.get_main_option("sqlalchemy.url")
 
 
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
+def do_run_migrations(connection):
     context.configure(
-        url=url,
+        connection=connection,
         target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        compare_type=True,
     )
-
     with context.begin_transaction():
         context.run_migrations()
 
 
-def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)
-
-    with context.begin_transaction():
-        schema = config.get_main_option("schema")
-        if schema is not None:
-            context.execute(f"CREATE SCHEMA IF NOT EXISTS {schema};")
-            context.execute(f"SET search_path TO {schema}")
-        context.run_migrations()
-
-
-async def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = AsyncEngine(
-        engine_from_config(
-            config.get_section(config.config_ini_section),
-            prefix="sqlalchemy.",
-            poolclass=pool.NullPool,
-            future=True,
-        )
-    )
-
+async def run_async_migrations(connectable):
     async with connectable.connect() as connection:
+        await connection.execute(text(f"SET search_path TO {schema}"))
         await connection.run_sync(do_run_migrations)
-
+        await connection.commit()
     await connectable.dispose()
 
 
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    asyncio.run(run_migrations_online())
+def run_migrations_online():
+    connectable = context.config.attributes.get("connection", None)
+    if connectable is None:
+        connectable = AsyncEngine(
+            engine_from_config(
+                context.config.get_section(context.config.config_ini_section),
+                prefix="sqlalchemy.",
+                poolclass=pool.NullPool,
+                future=True,
+                echo=False,
+            ),
+        )
+
+    if isinstance(connectable, AsyncEngine):
+        asyncio.run(run_async_migrations(connectable))
+    else:
+        do_run_migrations(connectable)
+
+
+run_migrations_online()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,14 @@
+import os
 from typing import AsyncGenerator
 
 import pytest_asyncio
+from alembic.config import Config
 from httpx import AsyncClient
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
+from alembic import command
 from src.config import get_settings
 from src.infrastructure.datasource.database import get_db_async
 from src.main import app, verify_token
@@ -13,8 +17,36 @@ app.dependency_overrides[verify_token] = lambda: None
 app.dependency_overrides[get_db_async] = lambda: None
 
 
+_path = os.path.dirname(__file__)
+__config_path__ = _path + "/alembic-test.ini"
+__migration_path__ = _path + "/../alembic/dev/"
+
+cfg = Config(__config_path__)
+cfg.set_main_option("script_location", __migration_path__)
+
+
+def __execute_downgrade(connection: AsyncSession) -> None:
+    cfg.attributes["connection"] = connection
+    command.downgrade(cfg, "base")
+
+
+def __execute_upgrade(connection: AsyncSession) -> None:
+    cfg.attributes["connection"] = connection
+    command.upgrade(cfg, "head")
+
+
+async def migrate_db(conn_url: str) -> None:
+    async_engine = create_async_engine(conn_url, echo=True)
+    async with async_engine.begin() as conn:
+        await conn.execute(text(f"SET search_path TO {get_settings().DATABASE_SCHEMA}"))
+        await conn.run_sync(__execute_downgrade)
+        await conn.run_sync(__execute_upgrade)
+
+
 @pytest_asyncio.fixture(scope="function")
 async def session() -> AsyncGenerator:
+    await migrate_db(get_settings().DATABASE_URL)
+
     async_engine = create_async_engine(get_settings().DATABASE_URL, echo=False)
 
     AsyncSessionLocal = sessionmaker(
