60c3c794e0d5d6664dcbd0943ee21646e69ddc7d
Ryosuke Nezaki <r.nezaki@gmail.com>
2022-12-30 16:13:11 +0900

    no message

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import logging
 import time
 from datetime import datetime
-from typing import Callable, Dict
+from typing import Callable
 
 import uvicorn
 from starlette.exceptions import HTTPException as StarletteHTTPException
